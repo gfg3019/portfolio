5823863387359559c68be0318d746fbe67e9241a
Genisson Gomes <genissongomes@gmail.com>
2022-03-31 13:44:49 -0300

    finalizando o site

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Nesse disafio foram usadas as tecnologias:
  - HTML5
  - CSS3
 ## fotos demonstrativas do projeto
+<img src="./assets/img1.png">
+<br>
+<br>
+<br>
+<img src="./assets/img2.png">
--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,16 @@
 const url = 'https://api.github.com/users/gfg3019'
-const imagens = document.querySelectorAll('.image')
 
-function sampleKeyData(){
+
+function showUserImage(){
     fetch(url)
         .then(res => res.json())
         .then(data => {
-            userName.textContent = data.name
             imageTitle.src = data.avatar_url
-            
+            imagePost.src = data.avatar_url
         })
         .catch(err => console.error(err))
 }
 
-sampleKeyData()
+
+
+showUserImage()
